6dcbaf3230dae2c13d33ac197549a606d3f8ac48
Vishwajeet Oraon <161809942+Vishhhwajeet007@users.noreply.github.com>
2025-09-26 20:01:05 +0530

    Add files via upload

---
+++ SQL Queries/Query1 (1).sql
@@ -0,0 +1,46 @@
+-- Total Amount by Payment Type
+SELECT payment_type_name, ROUND(SUM(total_amount),2) AS Total_amount
+FROM `uber-data-analysis-473212.Uber_dataset.tbl_analysis_report`
+GROUP BY payment_type_name
+ORDER BY total_amount DESC;
+
+-- Average Trip Distance by Payment Type
+SELECT payment_type_name, ROUND(AVG(total_amount),2) AS distance
+FROM `uber-data-analysis-473212.Uber_dataset.tbl_analysis_report`
+GROUP BY payment_type_name
+ORDER BY distance DESC;
+
+-- Total Amount by Rate code
+SELECT rate_code_name, ROUND(SUM(total_amount),2) AS total_amount
+FROM `uber-data-analysis-473212.Uber_dataset.tbl_analysis_report`
+GROUP BY rate_code_name
+ORDER BY total_amount DESC;
+
+-- Total Amount by Passenger Count
+SELECT passenger_count, ROUND(SUM(total_amount),2) AS Total_amount
+FROM `uber-data-analysis-473212.Uber_dataset.tbl_analysis_report`
+GROUP BY passenger_count
+ORDER BY total_amount DESC;
+
+-- Total Tip amount by Passenger count
+SELECT passenger_count, ROUND(SUM(tip_amount),2) AS Total_amount
+FROM `uber-data-analysis-473212.Uber_dataset.tbl_analysis_report`
+GROUP BY passenger_count
+ORDER BY total_amount DESC;
+
+-- Average Fare and Tip by Payment Type
+SELECT
+  pt.payment_type_name,
+  COUNT(ft.trip_id) AS total_trips,
+  AVG(ft.fare_amount) AS avg_fare,
+  AVG(ft.tip_amount) AS avg_tip,
+  SUM(ft.total_amount) AS total_fare_and_tip
+FROM
+  `uber-data-analysis-473212.Uber_dataset.fact_table` AS ft
+JOIN
+  `uber-data-analysis-473212.Uber_dataset.payment_type_dim` AS pt
+  ON ft.payment_type_id = pt.payment_type_id
+GROUP BY
+  1
+ORDER BY
+  total_fare_and_tip DESC;
